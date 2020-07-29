c9531cba36de6545795152bf7c6b2b7a7e27a754
Cthulhu <jaxa5963@gmail.com>
2020-07-29 18:20:45 +0900

    初期状態

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import firebase from 'firebase/app'
+import "./assets/css/tailwind.css"
+
+Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBp2wjBBIdDOlSuNfi5o_ego-i4_fHkVpg",
+  authDomain: "slack-clone-5fecc.firebaseapp.com",
+  databaseURL: "https://slack-clone-5fecc.firebaseio.com",
+  projectId: "slack-clone-5fecc",
+  storageBucket: "slack-clone-5fecc.appspot.com",
+  messagingSenderId: "46243161",
+  appId: "1:46243161:web:01eb6e23d6726c70300048",
+  measurementId: "G-H5R626JJXJ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Register from '../views/Register.vue'
+import SingIn from '../views/SignIn.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register
+  },
+  {
+    path: "/signin",
+    name: "signIn",
+    component: Signin
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
